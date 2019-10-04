0392cac7bf59aa54818f9736937625032283dd69
cuongld2 <ledinhcuong99@gmail.com>
2019-10-04 11:27:47 +0700

    set username variable minor change

--- practice/api/test_simple_blog_api.py
+++ practice/api/test_simple_blog_api.py
@@ -8,13 +8,14 @@ class TestUserInfo:
     restapi_query = RestAPIDatabase()
 
     def test_create_new_user_success(self, set_up_mysql):
-        response = self.user_api.create_new_user('test1990@gmail.com', 'Abcd1234', 'Le Dinh Cuong')
+        username = 'test1990@gmail.com'
+        response = self.user_api.create_new_user(username, 'Abcd1234', 'Le Dinh Cuong')
         try:
-            result_user_info = self.restapi_query.get_user_info_by_username(set_up_mysql, 'test1990@gmail.com')
+            result_user_info = self.restapi_query.get_user_info_by_username(set_up_mysql, username)
             assert result_user_info is not None
             assert 200 == response.status_code
         finally:
-            self.restapi_query.delete_user_info_by_username(set_up_mysql, 'test1990@gmail.com')
+            self.restapi_query.delete_user_info_by_username(set_up_mysql, username)
 
 
 
