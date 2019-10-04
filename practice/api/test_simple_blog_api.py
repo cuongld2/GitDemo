e42848e243d8e461aec321db026d9609acf34f50
cuongld2 <ledinhcuong99@gmail.com>
2019-10-04 17:57:43 +0700

    add random generator for string remove unnecessary test

--- practice/api/test_simple_blog_api.py
+++ practice/api/test_simple_blog_api.py
@@ -1,14 +1,17 @@
 
 from api.blog.user import User
 from utils_customs.database import RestAPIDatabase
+from utils_customs.random import RandomGenerator
 
 
 class TestUserInfo:
     user_api = User()
     restapi_query = RestAPIDatabase()
+    random_gen = RandomGenerator()
 
     def test_create_new_user_success(self, set_up_mysql):
-        username = 'test1990@gmail.com'
+        username = self.random_gen.random_string(8) + '@gmail.com'
+        # username = 'test1990@gmail.com'
         response = self.user_api.create_new_user(username, 'Abcd1234', 'Le Dinh Cuong')
         try:
             result_user_info = self.restapi_query.get_user_info_by_username(set_up_mysql, username)
--- practice/test_pytest.py
+++ practice/test_pytest.py
@@ -22,11 +22,6 @@ def test_print_name_2(pytestconfig):
     print(f"test_print_name_2(name): {pytestconfig.getoption('name')}")
 
 
-def test_read_yaml_file():
-    data_yaml = yaml.read_data_from_file('../resources/env.local.yaml')
-    print('api server url is : ' + data_yaml['api_server_url'])
-
-
 def test_print():
     if path.exists("guru.txt"):
         shutil.copyfile('guru.txt', 'test.txt')
